93834adfadc8df771dd55e29a8a102051205671f
SupernovaXTS <dylanhermangamer@gmail.com>
2024-11-16 12:34:53 -0500

    comments

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -12,6 +12,7 @@ echo "usb_f_rndis" | sudo tee -a /etc/modules
 curl -fsSL https://tailscale.com/install.sh | sh
 curl https://raw.githubusercontent.com/virtualhere/script/main/install_server | sh
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+# Node install
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
@@ -20,6 +21,7 @@ nvm install 11
 sudo setcap cap_net_bind_service=+ep `readlink -f \`which node\``
 npm install --global node-gyp@8.4.1
 npm config set node_gyp $(npm prefix -g)/lib/node_modules/node-gyp/bin/node-gyp.js
+# Grab depends for program
 npm install
 
 cp usb_setup_script.sh /usr/local/bin/toypad_usb_setup.sh
